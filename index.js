ada53591c25c6126debc1b1cd2ed678309638a92
Subhankar <maitisubhankar55@gmail.com>
2025-12-04 21:46:40 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function updateMap(){
     fetch("/data.json")
     .then(response => response.json())
-    .then(rsp => console.log(rsp.data))
+    .then(rsp => {
+        console.log(rsp); 
+        rsp.data.forEach(element => {
+            latitude = element.latitude;
+            longitude = element.longitude;
+            cases = element.infected;
+            let color;
+            if(cases > 255){
+                color = "rgb(255,0,0)";
+            }else{
+                color = `rgb(${cases},0,0)`;
+            }
+            // create a marker at a coordinate
+            new mapboxgl.Marker({
+                draggable: false,
+                color: color
+            })
+            .setLngLat([longitude, latitude])
+            .addTo(map);
+    }) 
+    })             
 }
 
 updateMap();
